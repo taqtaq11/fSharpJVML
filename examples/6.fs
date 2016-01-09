239399232e9fa7803488411cbd1caaa1869c3cb8
taqtaq11 <taqtaq11@gmail.com>
2016-01-09 13:34:01 +0300

    codgen addtions

---
+++ examples/6.fs
@@ -0,0 +1,16 @@
+let f x y = 
+    begin
+    if x > y then
+        begin
+        2
+        end
+    else
+        begin
+        3
+        end
+    end
+
+let main args =
+    begin
+    f(1)
+    end
---
+++ fSharpJVML/fsCompiler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Antlr.Runtime;
+using Antlr.Runtime.Tree;
+using fsharp_ss;
+
+namespace fSharpJVML
+{
+    public class fsCompiler
+    {
+        private string source;
+        private ITree sourceTree;
+
+
+        public ITree SourceTree
+        {
+            get
+            {
+                return sourceTree;
+            }
+        }
+
+
+        public fsCompiler() { }
+
+        public fsCompiler(string source)
+        {
+            this.source = source;
+        }
+
+
+        public void CreateTree()
+        {
+            fsharp_ssLexer lexer = new fsharp_ssLexer(new ANTLRStringStream(source));
+            var tokens = new CommonTokenStream(lexer);
+            fsharp_ssParser parser = new fsharp_ssParser(tokens);
+            sourceTree = (ITree)parser.execute().Tree;
+        }
+
+        public void InferTypes()
+        {
+            fsTypeInferer ti = new fsTypeInferer(SourceTree);
+            sourceTree = ti.Infer();
+        }
+
+        public void GenerateCode(string outputFilesPath, string outputFileName)
+        {
+            fsCodeGenerator cg = new fsCodeGenerator(outputFilesPath, outputFileName);
+            cg.GenerateClassFiles(SourceTree);
+        }
+    }
+}
